0324971f2ba01f42e506328f2c068af18864654a
Cam <ctucker@regent-college.edu>
2023-12-18 11:03:22 -0600

    Update HttpConnector.php don't change signature

--- src/Beanstream/communications/HttpConnector.php
+++ src/Beanstream/communications/HttpConnector.php
@@ -53,7 +53,7 @@ class HttpConnector {
      * @throws ApiException
      * @throws ConnectorException
      */
-    private function request($url, $http_method = NULL, $data = NULL)
+    private function request($http_method = NULL, $url = '', $data = NULL)
     {
     	//check to see if we have curl installed on the server 
         if ( ! extension_loaded('curl')) {
